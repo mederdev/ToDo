73a6058dd7ca988086ef1fc9d22ede2043c4b6b3
1MEDO1 <69678529+1MEDO1@users.noreply.github.com>
2021-01-15 19:59:49 +0600

    init

---
+++ main/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,HttpResponse
+
+def homepage(request):
+    return HttpResponse("Hello world")
+
+
+def test(request):
+    return render(request,"test.html")
+
+def go(request):
+    return HttpResponse("This is my first page")
+
