b0d68aa916ffed821706ef15264b3000e8490f5c
1MEDO1 <69678529+1MEDO1@users.noreply.github.com>
2021-01-19 21:45:10 +0600

    set media and static urls and roots

--- main/views.py
+++ main/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render,HttpResponse
 
 def homepage(request):
-    return HttpResponse("Hello world")
+    return render(request,"index.html")
 
 
 def test(request):
     return render(request,"test.html")
 
-def go(request):
-    return HttpResponse("This is my first page")
 
 def index(request):
     return render(request,"index.html")
