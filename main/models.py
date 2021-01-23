2766cedd3072d73b4aaf712bf914ef66b8186d8b
1MEDO1 <69678529+1MEDO1@users.noreply.github.com>
2021-01-23 18:54:19 +0600

    added todo form

--- main/models.py
+++ main/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class ToDo(models.Model):
     text=models.CharField(max_length=100)
-    created_at=models.DateField(auto_now_add=True)
+    created_at=models.DateTimeField(auto_now_add=True)
     is_closed=models.BooleanField(default=False)
     is_favotite=models.BooleanField(default=False)
--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from .models import ToDo
 def homepage(request):
     return render(request,"index.html")
@@ -11,3 +11,11 @@ def test(request):
 
 def index(request):
     return render(request,"index.html")
+
+
+def add_todo(request):
+    form=request.POST
+    text=form["todo_text"]
+    todo=ToDo(text=text)
+    todo.save()
+    return redirect(test)
